528efd4bd791fde105e5d195bf6ecec17d85746e
Bri Turner <blturner3527@gmail.com>
2020-01-14 04:25:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,8 @@
-# My Code here....
+def map_to_negativize(source_array)
+  index = 0
+  new_arr = []
+  while index < source_array.length do
+    new_arr << source_array[i] * -1
+    index += 1
+  end
+end
