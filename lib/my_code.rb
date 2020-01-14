6c3e5302ac512a7dfde2b78b3f2e61a3446ceebe
Bri Turner <blturner3527@gmail.com>
2020-01-14 04:33:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -65,12 +65,12 @@ def reduce_to_any_true(source_array)
   index = 0
   
   while index < source_array.length do
-    if source_array[index] == false
-      return false
+    if source_array[index] == true
+      return true
     end
     index += 1
   end
-  true
+  false
 end
 
 
