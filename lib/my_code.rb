27d83e24cc39a5be3a63088252c6fba9089fc702
Bri Turner <blturner3527@gmail.com>
2020-01-14 04:27:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def map_to_no_change(source_array)
   index = 0
   new_arr = []
   while index < source_array.length do
-    new_arr << (source_array[index] * -1)
+    new_arr << source_array[index]
     index += 1
   end
   new_arr
